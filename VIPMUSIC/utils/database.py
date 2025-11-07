b07a6701519126894afb5961c4a9aab28294586c
GhosttBatt <ghosttbatt@gmail.com>
2025-11-07 20:55:05 +0530

    database.py

--- VIPMUSIC/utils/database.py
+++ VIPMUSIC/utils/database.py
@@ -4,6 +4,14 @@ from typing import Dict, List, Union
 from VIPMUSIC import userbot
 from VIPMUSIC.core.mongo import mongodb
 
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI, MONGO_DB_NAME
+
+# Initialize MongoDB client
+_mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
+mongodb = _mongo_client[MONGO_DB_NAME]
+
+
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
 autoenddb = mongodb.autoend
