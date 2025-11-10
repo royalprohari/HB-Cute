f6497f72937ccb93de6b88cd26191cddba2feb4c
GhosttBatt <ghosttbatt@gmail.com>
2025-11-10 07:34:45 +0530

    zzztest.py

--- VIPMUSIC/plugins/tools/zzztest.py
+++ VIPMUSIC/plugins/tools/zzztest.py
@@ -1,9 +1,15 @@
 from VIPMUSIC import app
 from pyrogram import filters
+from VIPMUSIC.misc import SUDOERS
 
 print("[ReactionBot] Plugin loaded!")
 
-@app.on_message(filters.command("zzztest") & filters.group)
+@app.on_message(filters.command("zzztest", prefixes="/") & SUDOERS)
 async def zzztest_cmd(_, message):
     print("[ReactionBot] /zzztest command triggered!")
     await message.reply_text("✅ ZZZ Test command works!")
+
+@app.on_message(filters.command("reactiontest", prefixes="/") & SUDOERS)
+async def reactiontest_cmd(_, message):
+    print("[ReactionBot] /reactiontest command triggered!")
+    await message.reply_text("✅ Reaction test command works!")
