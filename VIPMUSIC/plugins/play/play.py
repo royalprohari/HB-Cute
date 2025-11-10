6219cc2d265aba923a874cb21160ffd1d3fe6f94
GhosttBatt <ghosttbatt@gmail.com>
2025-11-10 22:10:29 +0530

    play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -34,6 +34,8 @@ from config import BANNED_USERS, lyrical
 from time import time
 from VIPMUSIC.utils.extraction import extract_user
 
+print("[play] play, vplay, cplay", cvplay, playforce, cvplayforce)
+
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
 user_command_count = {}
@@ -1159,5 +1161,6 @@ async def get_thumb(vidid):
     except Exception as e:
         return config.YOUTUBE_IMG_URL
     
+
 
 
