cd315f47fe1a179e8db4ac0838875eea8881db83
GhosttBatt <ghosttbatt@gmail.com>
2025-11-13 19:13:02 +0530

    queue.py

--- VIPMUSIC/utils/inline/queue.py
+++ VIPMUSIC/utils/inline/queue.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from config
 
 def queue_markup(
     _,
@@ -69,15 +69,12 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-
-        ],
-        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_6"], url="https://t.me/HeartBeat_FAM",
+                text=_["S_B_6"], url=f"https://t.me/{SUPPORT_CHAT}"),
                 
             ),
         ],
@@ -97,26 +94,15 @@ def queuemarkup(_, vidid, chat_id):
 
 
         [
-            InlineKeyboardButton(
-                text="II",
-                callback_data=f"ADMIN Pause|{chat_id}",
-            ),
-
-            InlineKeyboardButton(
-                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
-            ),
-
-            InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
-            ),
-        ],
-        [
+            InlineKeyboardButton(text="II",callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_6"], url="https://t.me/HeartBeat_FAM",
+                text=_["S_B_6"], url=f"https://t.me/{SUPPORT_CHAT}"),
                 
             ),
         ],
